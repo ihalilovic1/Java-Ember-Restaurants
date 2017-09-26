fedb1d9d36d9c137550342677a893667cf8cf9fe
Irhad Halilovic <irhadhalilovic@hotmail.com>
2017-09-26 16:12:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java-Ember-Restaurants
---
+++ app/controllers/Application.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+public class Application extends Controller {
+
+    public Result test() {
+        return ok("RADIIII");
+    }
+
+}
