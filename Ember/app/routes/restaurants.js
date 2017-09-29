6308ebe1ce54e0aae1a42839d410641d25883dc7
Irhad Halilovic <irhadhalilovic@hotmail.com>
2017-09-29 23:40:53 +0200

    Header&Footer

--- Ember/app/routes/restaurants.js
+++ Ember/app/routes/restaurants.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     ajax: Ember.inject.service(),
     model() {
-        return this.get('ajax').request('/test');
+        //return this.get('ajax').request('/test');
     }
 });
