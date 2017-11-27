b7239c547e86ce4ffac12f1013d0a6c28e4b5cd2
Irhad Halilovic <irhadhalilovic@hotmail.com>
2017-11-27 01:17:24 +0100

    added index route

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.Result;
+
+import views.html.*;
+
+
+public class HomeController extends AbstractController {
+
+    public Result index(String slug) {
+        return ok(index.render());
+    }
+}
